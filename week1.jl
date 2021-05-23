9c94e166faa8e085f9581869ee1e036b92047f2c
Dinesh-Adhithya-H <68704516+Dinesh-Adhithya-H@users.noreply.github.com>
2021-05-24 00:33:37 +0530

    Add files via upload

---
+++ week1.jl
@@ -0,0 +1,226 @@
+@time print("lol")
+
+@time println("lol")
+
+1/2+3/4
+
+1/(2+3)/4
+
+2^3
+
+2.
+
+@time print("lol")
+
+print(2/3^2)
+
+2+3/6.7
+
+0.2+0.1-5.25
+
+2+Inf
+
+!true
+
+!false
+
+true && true
+
+true || false
+
+1<22
+
+-10<3
+
+2==4-2.0
+
+3==3.0
+
+3-4>1 && 2+2==5 || 5>2
+
+3>0 || p
+
+x=2
+
+3x
+
+3x^2
+
+3 & 0
+
+1 + 2 - 5 - 2 == 0 
+
+3 & 5 > 0 
+
+
+8 & 5 > 0
+
+typeof(1)
+
+typeof(1+2+3)
+
+typeof("H")
+
+typeof(1+2+3.)
+
+typeof('j')
+
+typeof(true)
+
+2<3/2
+
+2. == 2
+
+a=Array{Int32}(undef,3)
+
+a[1]=3
+
+a
+
+a=Array{String}(undef,3)
+
+a[1]="hazard"
+
+a[2]="benzema"
+
+a[3]="mbappe"
+
+a
+
+print(a[2])
+
+Array{Int64}(undef,2,3)
+
+'a'=="a"
+
+String
+
+x = Array{Int64}(undef,11, 12) 
+
+typeof(x)
+
+cos(0.3)
+
+log(3.14)
+
+log(10)
+
+abs(1.1)
+
+a=-1.1
+
+abs(a)
+
+?cos
+
+?FLoops
+
+?help
+
+?@time
+
+?
+
+b=deepcopy(a)
+
+a=2
+
+b
+
+c=a
+
+a=1.1
+
+c
+
+d=copy(a)
+
+a=3
+
+d
+
+methods(muladd)
+
+methods(cos)
+
+a=3
+
+myfunc(x)= x^2
+
+myfunc(5)
+
+f1(x,y)=x+y
+
+f1([2],[4])
+
+function blab(a,b,x)
+    return a*x+b
+end
+
+blab(2,3,5)
+
+function cool()
+    
+    print("lol")
+    print("bruh")
+end
+
+?print
+
+?println
+
+mycos(x::Float64)=cos(x)
+
+mycos(3.3)
+
+mycos(2)
+
+mycos(x::Int64)=cos(x)
+
+methods(mycos)
+
+x=7
+
+(x ≥ 5.0) && (x < 7.0)
+
+true && false && false || true
+
+true && ((false && false) || true)
+
+((true && false) && false) || true
+
+true && (false && (false || true))
+
+ (true && false) && (false || true)
+
+a, b = 2, 3           
+function f(x)  
+  b = 5 
+  a*x + b
+end
+f(7), b
+
+function f(x)
+  return 2x
+  3x
+end
+
+f(5)
+
+2^2^3 - 2^4
+
+println("$(6*6)")
+
+3^2^1^5 + 3^3
+
+(2^2)^3 - 2^4
+
+1 + 3 < 5 && 2^2 === 4.0
+
+true && ((false && false) || true)
+
+true && (false && (false || true))
+
+  ((true && false) && false) || true
+
+
